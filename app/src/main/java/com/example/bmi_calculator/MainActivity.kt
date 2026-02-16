d989380a931024dfdbef36f8daeff6304274736d
Atukitech <atukitech@gmail.com>
2026-02-16 12:48:24 +0600

    first commit

--- app/src/main/java/com/example/bmi_calculator/MainActivity.kt
+++ app/src/main/java/com/example/bmi_calculator/MainActivity.kt
@@ -1,11 +1,12 @@
 package com.example.bmi_calculator
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 import com.example.bmi_calculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,68 +17,84 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-     binding.calculateBtn.setOnClickListener {
-         val heightTv = binding.heightET.text.toString().trim()
-         val weightTv = binding.weightET.text.toString().trim()
-     /// Validation For HEIGHT
-         if (heightTv.isEmpty()){
-             binding.heightTIL.error= "Enter Height"
-             return@setOnClickListener
-         }else{
-             binding.heightTIL.error=null
-         }
-
-     //Validation for Weight
-         if (weightTv.isEmpty()){
-             binding.weightTIL.error= "Enter Weight"
-             return@setOnClickListener
-         }else{
-             binding.weightTIL.error=null
-         }
-         val height = heightTv.toDouble()
-         val weight = weightTv.toDouble()
-         if(height<=0){
-             binding.heightTIL.error= "Height Is Wrong"
-             return@setOnClickListener
-         }
-         //BMI CALCULATION
-         //Bmi Calculation
-         val bmi = weight/(height*height)
-
-         //result section
-         binding.title.parent?.let{
-             (it as View).visibility= View.VISIBLE
-         }
-         binding.bmiResult.text = String.format("%.2f",bmi)
-
-         //Bmi Status
-
-         when{
-             bmi<18.5 ->{
-                 binding.bmiStatus.text ="Underweight"
-                 binding.bmiAdvice.text="Eat more"
-             }
-             bmi in 18.5..24.9-> {
-                 binding.bmiStatus.text ="Normal"
-                 binding.bmiAdvice.text="Great!Please maintain"
-             }
-             bmi in 25.0..39.9-> {
-                 binding.bmiStatus.text ="Overweight"
-                 binding.bmiAdvice.text="Reduce Overweight"
-             }
-             bmi >40.0-> {
-                 binding.bmiStatus.text ="Obese"
-                 binding.bmiAdvice.text="Please maintain"
-             }
-         }
-
-         binding.bmiStatus.visibility= View.VISIBLE
-         binding.bmiAdvice.visibility= View.VISIBLE
-         //
+        //RESULT BINDING
+        val username = intent.getStringExtra("VALUE_NAME")
+        binding.resultTV.text = username
+        //RESULT AGE
+        val userAge: Int = intent.getIntExtra("AGE",0)
+        binding.resultTV2.text = userAge.toString()
 
 
-     }
+        /////////////////////////////////////////////////////////
+
+        //SIGHNOUT BUTTON
+        binding.signoutBtn.setOnClickListener {
+            Toast.makeText(this, "Signout button clicked", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this@MainActivity, WelcomePage::class.java))
+        }
+        binding.calculcateBtn.setOnClickListener {
+            val heightTv = binding.heightET.text.toString().trim()
+            val weightTv = binding.weightET.text.toString().trim()
+            /// Validation For HEIGHT
+            if (heightTv.isEmpty()) {
+                binding.heightTIL.error = "Enter Height"
+                return@setOnClickListener
+            } else {
+                binding.heightTIL.error = null
+            }
+
+            //Validation for Weight
+            if (weightTv.isEmpty()) {
+                binding.weightTIL.error = "Enter Weight"
+                return@setOnClickListener
+            } else {
+                binding.weightTIL.error = null
+            }
+            val height = heightTv.toDouble()
+            val weight = weightTv.toDouble()
+            if (height <= 0) {
+                binding.heightTIL.error = "Height Is Wrong"
+                return@setOnClickListener
+            }
+            //BMI CALCULATION
+            //Bmi Calculation
+            val bmi = weight / (height * height)
+
+            //result section
+            binding.title.parent?.let {
+                (it as View).visibility = View.VISIBLE
+            }
+            binding.bmiResult.text = String.format("%.2f", bmi)
+
+            //Bmi Status
+
+            when {
+                bmi < 18.5 -> {
+                    binding.bmiStatus.text = "Underweight"
+                    binding.bmiAdvice.text = "Eat more"
+                }
+
+                bmi in 18.5..24.9 -> {
+                    binding.bmiStatus.text = "Normal"
+                    binding.bmiAdvice.text = "Great!Please maintain"
+                }
+
+                bmi in 25.0..39.9 -> {
+                    binding.bmiStatus.text = "Overweight"
+                    binding.bmiAdvice.text = "Reduce Overweight"
+                }
+
+                bmi > 40.0 -> {
+                    binding.bmiStatus.text = "Obese"
+                    binding.bmiAdvice.text = "Please maintain"
+                }
+            }
+
+            binding.bmiStatus.visibility = View.VISIBLE
+            binding.bmiAdvice.visibility = View.VISIBLE
+
+
+        }
 
     }
 }
--- app/src/main/java/com/example/bmi_calculator/WelcomePage.kt
+++ app/src/main/java/com/example/bmi_calculator/WelcomePage.kt
@@ -2,26 +2,41 @@ package com.example.bmi_calculator
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.bmi_calculator.databinding.ActivityMainBinding
 import com.example.bmi_calculator.databinding.ActivityWelcomePageBinding
 
 class WelcomePage : AppCompatActivity() {
-    private  lateinit var binding: ActivityWelcomePageBinding
+    private lateinit var binding: ActivityWelcomePageBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding= ActivityWelcomePageBinding.inflate(layoutInflater)
+        binding = ActivityWelcomePageBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.nextBtn.setOnClickListener {
-            Toast.makeText(this, "Next button clicked", Toast.LENGTH_SHORT).show()
+            val name = binding.nameET.text.toString().trim()
 
-            startActivity(Intent(this@WelcomePage, MainActivity::class.java))
+            val age = binding.ageET.text.toString().trim().toInt()
+
+            if (name.isEmpty()) {
+                binding.nameTIL.error = "Enter Name"
+                return@setOnClickListener
+            } else {
+                binding.nameTIL.error = null
+            }
+
+            val intent = Intent(this@WelcomePage, MainActivity::class.java)
+            intent.putExtra("VALUE_NAME", name)
+            intent.putExtra("AGE", age)
+            startActivity(intent)
         }
-
     }
 }
+
+
+
+
+
+
+
+
